2b3d670d28ea89a235768187230475ffabfc9de4
MaksimMaltsev <m.maltsev@dsec.com>
2022-12-14 23:21:54 +0300

    insert html in JS

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,13 +1,17 @@
+import {$} from '@core/dom';
+
 export class Excel {
     constructor(selector, options) {
         this.$el = document.querySelector(selector)
         this.components = options.components || []
     }
     getRoot() {
-        const $root = document.createElement('div')
+        const $root = $.create('div', 'excel')
         this.components.forEach(Component => {
-            const component = new Component()
-            $root.insertAdjacentHTML('beforeend', component.toHTML() )
+            const $el = $.create('div', Component.className)
+            const component = new Component($el)
+            $el.innerHTML = component.toHTML()
+            $root.append($el)
         })
         return $root
     }

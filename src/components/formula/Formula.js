30dff7d68aaf94ea5072bfbdc31bed3b0d56cac8
MaksimMaltsev <m.maltsev@dsec.com>
2022-12-16 15:21:56 +0300

    add listeners methods

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -2,8 +2,17 @@ import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Formula extends ExcelComponent {
     static className = 'excel__formula'
+    constructor($root) {
+        super($root, {
+            name: 'Formula',
+            listeners: ['input']
+        });
+    }
     toHTML() {
         return `<div class="info">fx</div>
       <div class="input" contenteditable spellcheck="false"></div>`
+    }
+    onInput() {
+        console.log('onInput Formula', event)
     }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,14 @@
 import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {} ) {
+    super($root, options.listeners );
+  }
   // return component layout
   toHTML() {
     return ''
+  }
+  init() {
+    this.initDomListeners()
   }
 }

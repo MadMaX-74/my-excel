22b648ad8d7e3c8577e5e2ede7672dec5bf130cc
MaksimMaltsev <m.maltsev@dsec.com>
2022-12-25 21:27:26 +0300

    resize cols and rows working

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
+import {$} from '@core/dom';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -13,7 +14,26 @@ export class Table extends ExcelComponent {
     }
     onMousedown(event) {
         if (event.target.dataset.resize) {
-            console.log('Start resize')
+            const $resizer = $(event.target)
+            const $parent = $resizer.closest('[data-type="resizable"]')
+            const cords = $parent.getCoords()
+            const type = $resizer.data.resize
+            const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+            document.onmousemove = (e) => {
+                if (type === 'col') {
+                    const delta = e.pageX - cords.right
+                    const value = cords.width + delta
+                    $parent.$el.style.width = value + 'px'
+                    cells .forEach(el => el.style.width = value + 'px')
+                } else {
+                    const delta = e.pageY - cords.bottom
+                    const value = cords.height + delta
+                    $parent.$el.style.height = value + 'px'
+                }
+            }
+            document.onmouseup = () => {
+                document.onmousemove = null
+            }
         }
     }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -5,18 +5,19 @@ const CODES = {
 function toChar(_, idx) {
     return String.fromCharCode(CODES.A + idx)
 }
-function createCell() {
-    return `<div class="cell" contenteditable></div>`
+function createCell(_, col) {
+    return `<div class="cell" contenteditable data-col="${col}"></div>`
 }
-function createColumn(column) {
-    return `<div class="column">${column} 
+function createColumn(column, index) {
+    // eslint-disable-next-line max-len
+    return `<div class="column" data-type="resizable" data-col="${index}">${column} 
                 <div class="col-resize" data-resize="col"></div>
             </div>`
 }
 function createRow(index, content) {
     // eslint-disable-next-line max-len
     const resizer = index ? '<div class="row-resize" data-resize="row"></div>' : ''
-    return `<div class="row">
+    return `<div class="row" data-type="resizable">
         <div class="row-info">
             ${index ? index : ''}
             ${resizer}
@@ -30,7 +31,7 @@ export function createTable(rowsCount = 15) {
     const cols = new Array(colsCount)
         .fill('')
         .map(toChar)
-        .map(el => createColumn(el))
+        .map((el, index) => createColumn(el, index))
         .join('')
     rows.push(createRow(null, cols))
     for (let i = 0; i < rowsCount; i++) {

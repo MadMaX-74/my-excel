10208d065cbb2cb8e067ad95d4061cf1d5266c5c
MaksimMaltsev <m.maltsev@dsec.com>
2023-01-07 19:09:15 +0300

    add emtts to proj

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -12,7 +12,7 @@ export class Table extends ExcelComponent {
     constructor($root, options) {
         super($root, {
             name: 'Table',
-            listeners: ['mousedown', 'keydown'],
+            listeners: ['mousedown', 'keydown', 'input'],
             ...options
         });
     }
@@ -26,10 +26,17 @@ export class Table extends ExcelComponent {
     init() {
         super.init()
         const $cell = this.$root.find('[data-id="0:0"')
-        this.selection.select($cell)
+        this.selectCell($cell)
         this.$on('formula:input', text => {
             this.selection.current.text(text)
         })
+        this.$on('formula:done', () => {
+            this.selection.current.focus()
+        })
+    }
+    selectCell($cell) {
+        this.selection.select($cell)
+        this.$emit('table:select', $cell)
     }
     onMousedown(event) {
         if (shouldResize(event)) {
@@ -58,8 +65,11 @@ export class Table extends ExcelComponent {
             event.preventDefault()
             const id = this.selection.current.id(true)
             const $next = this.$root.find(nextSelector(key, id))
-            this.selection.select($next)
+            this.selectCell($next)
         }
+    }
+    onInput(event) {
+        this.$emit('table:input', $(event.target))
     }
 }
 
--- src/core/dom.js
+++ src/core/dom.js
@@ -12,7 +12,14 @@ class Dom {
         return this.$el.outerHTML.trim()
     }
     text(text) {
-        this.$el.textContent = text
+        if (typeof text === 'string') {
+            this.$el.textContent = text
+            return this
+        }
+        if (this.$el.tagName.toLowerCase() === 'input') {
+            return this.$el.value.trim()
+        }
+        return this.$el.textContent.trim()
     }
     clear() {
         this.html('')

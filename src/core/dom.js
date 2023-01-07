5d534b27544b1a3d7aacf2da45bb7cf23e1e09e5
MaksimMaltsev <m.maltsev@dsec.com>
2023-01-07 19:10:47 +0300

    add return to dom

--- src/core/dom.js
+++ src/core/dom.js
@@ -76,9 +76,11 @@ class Dom {
     }
     addClass(className) {
         this.$el.classList.add(className)
+        return this
     }
     removeClass(className) {
         this.$el.classList.remove(className)
+        return this
     }
 }
 

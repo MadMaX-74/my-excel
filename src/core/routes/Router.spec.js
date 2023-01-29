a13994919d454b321919c8ec06765e74dd5b32b4
MaksimMaltsev <m.maltsev@dsec.com>
2023-01-29 21:34:02 +0300

    test for router

---
+++ src/core/routes/Router.spec.js
@@ -0,0 +1,30 @@
+import {Router} from './Router';
+import {Page} from '../Page';
+
+class DashboardPage extends Page {
+    getRoot() {
+        const root = document.createElement('div')
+        root.innerHTML = 'dashboard'
+        return root
+    }
+}
+class ExcelPage extends Page {}
+
+describe('Router:', () => {
+    let router
+    let $root
+    beforeEach(() => {
+        $root = document.createElement('div')
+        router = new Router($root, {
+            dashboard: DashboardPage,
+            excel: ExcelPage
+        })
+    })
+    test('define', () => {
+        expect(router).toBeDefined()
+    })
+    test('render dashboardPage', () => {
+        router.changePageHandler()
+        expect($root.innerHTML).toBe('<div>dashboard</div>')
+    })
+})
